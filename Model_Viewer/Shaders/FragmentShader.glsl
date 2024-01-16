a8ddde9fe17d8f495ce23d09f8fcc83f9606894a
UmeshEkhande <umesh.ekhande@cctech.co.in>
2024-01-16 19:01:43 +0530

    Adding chaing.obj

--- Model_Viewer/Shaders/FragmentShader.glsl
+++ Model_Viewer/Shaders/FragmentShader.glsl
@@ -10,7 +10,7 @@ if(u_lightingEnabled==1)
 highp vec3 LightDir = normalize(lightPos - vert);
 highp float NL = max(dot(normalize(vertNormal), LightDir), 0.0);
 
-highp vec3 color = vec3(1.0, 1.0, 0.0);
+highp vec3 color = vec3(1.0, 0.0, .0);
 highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);
 
 gl_FragColor = vec4(col, 0.0);
